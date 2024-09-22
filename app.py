e662e48ca5dea92c785e8f1c188447e927c8055f
tawfik elmetwally <110994276+tawfikhammad@users.noreply.github.com>
2024-09-22 05:21:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import tempfile
-import os
 from summarizer import text_summarize
-from GetText import extract_text_from_pdf, file_text, wiki_text, extractOCR
+from GetText import extract_text_from_pdf, file_text, wiki_text
 
 def main():
     st.title('Text Summarizer')
@@ -14,14 +12,13 @@ def main():
     # User chooses the input method
     input_method = st.radio(
         "**Choose the input method**", 
-        ("Upload a text file", "Upload a PDF file", "Upload scanned PDF file")
+        ("Enter text directly", "Upload a PDF file")
     )
 
-    # If "Upload a text file"
-    if input_method == "Upload a text file":
-        uploaded_text_file = st.file_uploader("Choose a text file", type="txt")
-        if uploaded_text_file is not None:
-            st.session_state.text_input = file_text(uploaded_text_file)
+
+    # If "Enter text directly"
+    if input_method == "Enter text directly":
+        st.session_state.text_input = st.text_area("Enter text directly here")
 
     # If "Upload a PDF file"
     elif input_method == "Upload a PDF file":
@@ -29,21 +26,11 @@ def main():
         if uploaded_pdf_file is not None:
             st.session_state.text_input = extract_text_from_pdf(uploaded_pdf_file)
 
-    # If "Upload scanned PDF file"
-    elif input_method == "Upload scanned PDF file":
-        uploaded_pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
-        if uploaded_pdf_file is not None:
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-                temp_file.write(uploaded_pdf_file.read())
-                temp_pdf_path = temp_file.name
-            
-            st.session_state.text_input = extractOCR(temp_pdf_path, st.session_state.language)
-            os.remove(temp_pdf_path)  # Delete the temporary file after processing
 
     # Button to summarize
     if st.button('Summarize', key="summarize_button"):
         if st.session_state.text_input:
-            summary = text_summarize(st.session_state.text_input, st.session_state.language)
+            summary = text_summarize(st.session_state.text_input)
             st.write("Summarized Text:")
             st.write(summary)
         else:
