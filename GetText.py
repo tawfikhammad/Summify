0a96642af3c8765000d7c3b298c9ecb7639ae8cf
tawfik elmetwally <110994276+tawfikhammad@users.noreply.github.com>
2024-09-22 06:19:17 +0300

    Update GetText.py

--- GetText.py
+++ GetText.py
@@ -12,7 +12,7 @@ from bidi.algorithm import get_display
 import tempfile
 
 
-os.environ['TESSDATA_PREFIX'] = '/usr/share/tesseract-ocr/4.00/tessdata/'
+os.environ['TESSDATA_PREFIX'] = '/usr/share/tesseract-ocr/4.00/'
 pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 
 def is_scanned(uploaded_file):
