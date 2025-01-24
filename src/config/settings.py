d9287ccb25efba91515bb9c0278f9520f622081b
tawfikhammad <tawfikhammad@icloud.com>
2025-01-25 01:53:03 +0200

    create config wirh app settings

---
+++ src/config/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    
+    APP_NAME: str
+    APP_VERSION: str
+    ALLOWED_FILE_TYPES: list
+    MAX_FILE_SIZE: int
+    TESSERACT_CMD: str
+
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()
+
+
