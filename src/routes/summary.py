9af12dc79ebee09577f4e32e5af7311820a4a44b
tawfikhammad <tawfikhammad@icloud.com>
2025-01-28 18:02:38 +0200

    modify summary route (add upload schema request)

--- src/routes/summary.py
+++ src/routes/summary.py
@@ -1,12 +1,21 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, UploadFile, Depends, Form
 from controllers import SummaryController
+from .schemas import UploadRequest
+from helpers.enums import SummApproach
+
 
 summ_router = APIRouter()
 
-@summ_router.post('/data/summary')
-async def upload_file(file: UploadFile):
+def get_upload_request(summ_approach: SummApproach = Form(SummApproach.EXTRACTIVE), 
+                       max_length: int = Form(100),
+                       num_sentences: int = Form(1)) -> UploadRequest:
+
+    return UploadRequest(summ_approach = summ_approach, max_length = max_length, num_sentences = num_sentences)
+
+@summ_router.post('/data/summary/file')
+async def upload_file(file: UploadFile, upload_request: UploadRequest = Depends(get_upload_request)):
     
-    summary_controller = SummaryController(file)
+    summary_controller = SummaryController(file= file, upload_request= upload_request)
     summary = await summary_controller.get_summary()
     
     return summary
